9ffb732297a73cb16ffe083618fc51c3278df7b6
Jon Orton <jonorton66@gmail.com>
2017-10-19 21:05:45 -0400

    Creating a Searchable text file that will return to the program

--- WOD.py
+++ WOD.py
@@ -1,6 +1,12 @@
 path = 'WODList.txt'
+
+print('Hello')
+print('What Activity Do you want to do?')
+interest = raw_input()
+
 wod_file = open(path, 'r')
 wod = wod_file.read()
-print(wod)
+answer = wod.find(interest)
+print(answer)
 
 wod_file.close()
