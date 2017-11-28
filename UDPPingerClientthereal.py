a581e3d8be9c6f2b91abf1d8f88cef11853dc518
Jon Orton <30511293+jonorton66@users.noreply.github.com>
2017-11-27 21:53:36 -0500

    added chat client and server

---
+++ UDPPingerClientthereal.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from socket import *
+from time import time
+
+serverName = '10.252.100.56'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Message:')
+c = 10
+i = 1
+#v = (10 - 1)
+r = 9
+dt = datetime.now()
+dt2 = datetime.now()
+et = dt - dt2
+print c, 'pings are being tried.\n'
+while i < c:
+        i+= 1
+	r-= 1
+	#dt = datetime.now()
+	#dt2 = datetime.now()
+	et = (dt - dt2)
+        print '\nPing attempt number', i, 'is currently in progress.\n'
+        print r, 'attempts remain.\n'
+	#print v, 'attempts remain.\n
+        clientSocket.sendto(message,(serverName,serverPort))
+        clientSocket.settimeout(10)
+
+        print message
+        print 'Time Elapsed: ', et.microseconds, 'microseconds\n'
+        message, address = clientSocket.recvfrom(2048)
+timeout
+print 'Request Timed Out! '
+#while i <= 10:
+    #print counter, 'pings are being tried.\n'
+    #while i < counter:
+        #i+= 1
+    #print '\nPing attempt number', i, 'is currently in progress.\n'
+    #print remain, 'attempts remain.\n'
+    #dt = datetime.now()
+    #clientSocket.sendto(message,(serverName,serverPort))
+    #clientSocket.settimeout(1)
+
+    #print message
+    #print 'Time Elapsed: ', et.microseconds, 'microseconds\n'
+    #message, address = clientSocket.recvfrom(2048)
+    #dt2 = datetime.now()
+    ##et = dt - dt2;
+    #timeout
+    #print 'Request Timed Out! '
+#else:
+clientSocket.close()
+#pass
+#if __name__ == '__main__':
+        #main()
---
+++ UDPPingerServer.py
@@ -0,0 +1,25 @@
+# UDPPingerServer.py 
+# We will need the following module to generate randomized lost packets import random 
+from socket import * 
+import random
+# Create a UDP socket 
+# Notice the use of SOCK_DGRAM for UDP packets 
+serverSocket = socket(AF_INET, SOCK_DGRAM) # Assign IP address and port number to socket 
+serverSocket.bind(('', 12000)) 
+while 1: 
+    # Generate random number in the range of 0 to 10 
+    #rand = random.randint(0, 10) 
+    # Receive the client packet along with the address it is coming from 
+    message, address = serverSocket.recvfrom(2048)  
+    print message
+    print address
+# Capitalize the message from the client 
+    #ServerMessage = raw_input('Send what to the client:')
+    ServerMessage = 'Response'
+    print ServerMessage
+# If rand is less is than 4, we consider the packet lost and do not respond 
+   # if rand < 4: 
+        #continue 
+# Otherwise, the server responds 
+    #serverSocket.sendto(ServerMessage, address)
+    serverSocket.sendto(ServerMessage, address)
